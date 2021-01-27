58602d34b3c7f4af889dfe989ec91df81d8a7b5e
Andrei Stoian <andreiu_s@yahoo.com>
2021-01-27 16:32:36 +0200

    commit ex5 - metoda cu parametru

--- src/main/java/ex5.java
+++ src/main/java/ex5.java
@@ -1,9 +1,9 @@
 public class ex5 {
 
 
-   public static void doubleNo() {
+   public static void doubleNo(int[] List) {
 
-        int[] List = new int[]{5, 8, 11};
+        //int[] List = new int[]{5, 8, 11};
         for (int i = 0; i < List.length; i++) {
             List[i] = List[i] * 2;
             System.out.print(List[i] + " ");
@@ -13,8 +13,9 @@ public class ex5 {
 
 
     public static void main(String[] args){
+       int[] List = new int[]{5, 8, 11};
        System.out.print("Lista numerelor dublate este: ");
-       doubleNo();
+       doubleNo(List);
 
     }
 
