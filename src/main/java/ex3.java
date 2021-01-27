b69edaf497effc3881fca9ac19897b539f89844a
Andrei Stoian <andreiu_s@yahoo.com>
2021-01-27 15:23:09 +0200

    commit ex3

---
+++ src/main/java/ex3.java
@@ -0,0 +1,22 @@
+public class ex3 {
+
+    static int leapYear() {
+        int[] List = new int[]{13, 17, 20};
+        int i = 0;
+        int an = 2021;
+        while (i < 20) {
+            if ((an % 4) == 0) {
+                System.out.println(an);
+                an++;
+                i++;
+                } else {
+                    an++;
+                    }
+
+        }
+    return an;
+    }
+    public static void main(String[] args){
+        leapYear();
+    }
+}
