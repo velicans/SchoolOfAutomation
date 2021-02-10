ba987de14a36a4493925cff08dee97d51e93e010
alexandruratiuv <alexandru.ratiuv@gmail.com>
2021-02-10 19:32:56 +0200

    Commit homework Junit tests

--- src/main/java/seleniumtests/Selenium1.java
+++ src/main/java/seleniumtests/Selenium1.java
@@ -8,12 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOError;
 import java.util.List;
 
 public class Selenium1 {
@@ -21,7 +18,8 @@ public class Selenium1 {
 
     static WebDriver driver;
     static WebDriverWait wait;
-    static final String GRAPE_NAME="AlexR final3";
+    static final String GRAPE_NAME="AlexR final5";
+
 
     @BeforeAll
     public static void openBrowser() {
@@ -35,7 +33,44 @@ public class Selenium1 {
 
     }
 
+@Test
+@Disabled
+@DisplayName("testing stuff in wines")
+public void testingStuffInWines(){
 
+
+    WebElement table;
+    List<WebElement> rows;
+
+
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add grapes')]")));
+    driver.findElement(By.xpath("/html/body/div/div/header/nav/a[4]")).click();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/section/h3")));
+
+    table=driver.findElement(By.xpath("/html/body/div/div/section/table/tbody"));
+    rows = table.findElements(By.tagName("tr"));
+
+
+
+    for(WebElement row : rows){
+        List<WebElement> td = row.findElements(By.tagName("td"));
+
+        if (td.get(0).getText().equals(GRAPE_NAME)){
+            td.get(3).findElement(By.tagName("button")).click();
+            td.get(3).findElement(By.xpath("//td[contains(text(),'')]/input")).sendKeys("Test");
+            td.get(3).findElement(By.tagName("button")).click();
+
+            table=driver.findElement(By.xpath("/html/body/div/div/section/table/tbody"));
+            rows = table.findElements(By.tagName("tr"));
+
+
+            td.get(4).findElement(By.tagName("button")).click();
+            td.get(4).findElement(By.xpath("//td[contains(text(),'')]/input")).sendKeys("5");
+            td.get(4).findElement(By.tagName("button")).click();
+            break;
+        }
+    }
+}
     @Test
     @Disabled
     @DisplayName("testing stuff")
@@ -109,27 +144,20 @@ public class Selenium1 {
     public void grapeTest() {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(),'Add grapes')]")));
-
-
-
-        //Add new grape
-
         boolean findButtonCrush=false;
+        boolean findButtonFerment=false;
         WebElement table = driver.findElement(By.xpath("/html/body/div/div/section/table/tbody"));
         List<WebElement> rows = table.findElements(By.tagName("tr"));
 
 
-            driver.findElement(By.cssSelector(".animated-button")).click();
-            driver.findElement(By.cssSelector("#name")).sendKeys(GRAPE_NAME);
-            Select select = new Select(driver.findElement(By.cssSelector("#quantity")));
-            select.selectByVisibleText("24");
-            driver.findElement(By.id("age")).sendKeys("25");
-            driver.findElement(By.id("ripeness")).sendKeys("99");
-            driver.findElement(By.cssSelector(".add-grapes-form > input:nth-child(11)")).click();
-
-
-
-
+        //Add new grape
+        driver.findElement(By.cssSelector(".animated-button")).click();
+        driver.findElement(By.cssSelector("#name")).sendKeys(GRAPE_NAME);
+        Select select = new Select(driver.findElement(By.cssSelector("#quantity")));
+        select.selectByVisibleText("24");
+        driver.findElement(By.id("age")).sendKeys("25");
+        driver.findElement(By.id("ripeness")).sendKeys("99");
+        driver.findElement(By.cssSelector(".add-grapes-form > input:nth-child(11)")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".App-table")));
 
@@ -160,7 +188,7 @@ public class Selenium1 {
         table=driver.findElement(By.xpath("/html/body/div/div/section/table/tbody"));
         rows = table.findElements(By.tagName("tr"));
 
-        boolean findButtonFerment=false;
+
         for(WebElement row : rows){
             List<WebElement> td = row.findElements(By.tagName("td"));
             if (td.get(1).getText().equals(GRAPE_NAME)){
@@ -171,12 +199,11 @@ public class Selenium1 {
         }
 
 
-
-
-
+        //Check if can ferment
         Assert.assertTrue("Button not found",findButtonFerment);
-        driver.findElement(By.cssSelector(".App-page > button:nth-child(5)")).click();
 
+
+        driver.findElement(By.cssSelector(".App-page > button:nth-child(5)")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/section/h3")));
 
         try {
@@ -186,7 +213,40 @@ public class Selenium1 {
             Assert.fail("Wine not found");
         }
 
-    }
+
+
+
+        //Test set name/ set bottlingVolume
+        table=driver.findElement(By.xpath("/html/body/div/div/section/table/tbody"));
+        rows = table.findElements(By.tagName("tr"));
+
+
+        for(WebElement row : rows){
+            List<WebElement> td = row.findElements(By.tagName("td"));
+
+            if (td.get(0).getText().equals(GRAPE_NAME)){
+                td.get(3).findElement(By.tagName("button")).click();
+                td.get(3).findElement(By.xpath("//td[contains(text(),'')]/input")).sendKeys("Test");
+                td.get(3).findElement(By.tagName("button")).click();
+                break;
+            }
+        }
+
+        table=driver.findElement(By.xpath("/html/body/div/div/section/table/tbody"));
+        rows = table.findElements(By.tagName("tr"));
+
+        for(WebElement row : rows) {
+            List<WebElement> td = row.findElements(By.tagName("td"));
+            td.get(4).findElement(By.tagName("button")).click();
+            td.get(4).findElement(By.xpath("//td[contains(text(),'')]/input")).sendKeys("5");
+            td.get(4).findElement(By.tagName("button")).click();
+            break;
+        }
+
+
+
+
+        }
 
     @AfterAll
     public static void stopBrowser() {
